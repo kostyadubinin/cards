33db0325d6c9e080ce20e52cc15162311fde2383
Kostya Dubinin <kostya.dubinin@gmail.com>
2018-07-04 22:47:37 +0200

    Revert "Update puma config and docker compose"
    This reverts commit ca629ce29929cd7d030c89987b66f14ceaac4a50.

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,6 @@
-ssl_bind "0.0.0.0", "9292", {
-           key: "/mnt/volume_ams3_01/www_learnaword_net.key",
-           cert: "/mnt/volume_ams3_01/www_learnaword_net.pem"
-         }
+if !ENV['SSL_KEY_PATH'].nil? && !ENV['SSL_CERT_PATH'].nil?
+  ssl_bind "0.0.0.0", "9292", {
+             key: ENV['SSL_KEY_PATH'],
+             cert: ENV['SSL_CERT_PATH']
+           }
+end
