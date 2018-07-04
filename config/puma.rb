71f9a13f095593e26d508f6cee8d7ffa8bb289fa
Kostya Dubinin <kostya.dubinin@gmail.com>
2018-07-04 23:43:27 +0200

    Remove puma config

--- config/puma.rb
+++
@@ -1,6 +0,0 @@
-if !ENV['SSL_KEY_PATH'].nil? && !ENV['SSL_CERT_PATH'].nil?
-  ssl_bind "0.0.0.0", "9292", {
-             key: ENV['SSL_KEY_PATH'],
-             cert: ENV['SSL_CERT_PATH']
-           }
-end
