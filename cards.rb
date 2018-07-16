63c4b98d4cebb707f93d5e8d838a533f55d17136
Kostya Dubinin <kostya.dubinin@gmail.com>
2018-07-16 21:43:02 +0200

    Fix the password env variable

--- cards.rb
+++ cards.rb
@@ -8,7 +8,6 @@ require "redis"
 # TODO: Don't log tokens.
 
 use Rack::Auth::Basic, "Restricted Area" do |username, password|
-  logger.info("password path: #{ENV['PASSWORD_PATH'].inspect}")
   username == "admin" and password == File.read(ENV["PASSWORD_PATH"])
 end
 
